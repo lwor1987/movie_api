b583babf7877d0200e4abdec6519423e623b8b64
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2021-12-04 18:03:16 -0500

    Fixed PORT.

--- index.js
+++ index.js
@@ -259,7 +259,7 @@ app.use((err, req, res, next ) => {
 //app.listen(8080, () => {
  // console.log( "Listening on Port 8080.");
 //});
- const port = process.env.Port || 8080; 
-       app.listen(port, "0.0.0.0", () =>{
+ const port = process.env.PORT || 8080; 
+       app.listen(port, "0.0.0.0", () => {
          console.log("Listening on Port" + port);
        });
