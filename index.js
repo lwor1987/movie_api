4cc107a28e2b08696067a57c66374d8c776095f8
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-01-07 00:39:12 -0500

    Fixed error

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get("/genre/:name", passport.authenticate("jwt", {session:false}), (req, res
 app.get("/directors/:directorName", passport.authenticate("jwt", {session:false}), (req, res) =>{
   Movies.findOne({ "Director.Name": req.params.directorName})
     .then((movies) => {
-      res.status(201).json(movies.director);
+      res.status(201).json(movies.Director);
     })
     .catch((err) => {
       console.error(err);
