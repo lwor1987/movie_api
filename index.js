28c738b7af8ae7a043b07391834672482c2c61ae
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-02-02 22:46:20 -0500

    Added cors allowance.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 
 
 const cors = require("cors");
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'https://intense-ridge-76926.herokuapp.com', 'http://localhost:1234'];
 
 
 
