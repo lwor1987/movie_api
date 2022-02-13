1511ec1b1145e9f9a8b2bf6fd1186f91580483b6
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-02-13 01:49:52 -0500

    Deleted const cors.

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use (morgan("common"));
 app.use(express.static("public"));
 
 
-const cors = require("cors");
+
 let allowedOrigins = ['http://localhost:8080', 'https://intense-ridge-76926.herokuapp.com/', 'http://localhost:1234'];
 app.use(cors({
   origin: (origin, callback) => {
