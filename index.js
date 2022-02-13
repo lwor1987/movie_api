037031fb18e8dab536eede770a5b17cce9357280
lwor1987 <60717817+lwor1987@users.noreply.github.com>
2022-02-13 02:14:03 -0500

    Changed cors.

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const bodyParser = require("body-parser");
 
 const app = express(); 
 
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
+const cors = require('cors');
+
 const Models = require("./models.js");
 
 const { check, validationResult } = require("express-validator");
@@ -33,17 +35,7 @@ app.use(express.static("public"));
 
 
 
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 
 let auth = require("./auth")(app);
